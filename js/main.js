888e784b8ef9d100373a9de40eac83e20367ae4b
dmeskhi <dmeskh@gmail.com>
2018-02-25 00:03:18 -0700

    dmeskhi

--- js/main.js
+++ js/main.js
@@ -97,3 +97,18 @@ function outsideClick(e){
     player.pauseVideo();
   }
 }
+//Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 800);
+        return false;
+      }
+    }
+  });
+});
